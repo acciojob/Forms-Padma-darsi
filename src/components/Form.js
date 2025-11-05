346c9b2c7a5a139a3708f4fc642d4fe23e572571
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 02:13:04 +0530

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Card";
+
+const Form = () => {
+  return (
+    <Card>
+      <form id="info-form">
+        <div>
+          <label htmlFor="full_name">Full Name:</label>
+          <input type="text" id="full_name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
+        </div>
+        <div>
+          <label htmlFor="password_confirmation">Password Confirmation:</label>
+          <input type="password" id="password_confirmation" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </Card>
+  );
+};
+
+export default Form;
